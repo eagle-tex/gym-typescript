1ffd83ddbc24a50754ae7f1c5b5ce04a0c29436a
Gildas Montcho <gmontcho@gmail.com>
2023-09-23 00:46:33 +0200

    feat: setup tailwindcss & use in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="app">
+    <div className="app bg-gray-20">
       <h1>App</h1>
     </div>
   );
---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,27 @@
+// import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
+import { useState } from 'react';
+
+// import { Profile } from '../../components/profile/Profile';
+import { Asset } from '@assets/Asset';
+import Logo from '@assets/Logo.png';
+import { Profile } from '@components/profile/Profile';
+
+// import Logo from '../../assets/Logo.png';
+
+type Props = { dummy: string };
+
+function Navbar(props: Props) {
+  const { dummy } = props;
+
+  const flexBetween = 'flex items-center justify-between';
+
+  return (
+    <nav>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        Something {dummy}
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
