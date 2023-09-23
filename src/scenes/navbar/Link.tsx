9547130a75f574f61a49916fd384050bdfa20405
Gildas Montcho <gmontcho@gmail.com>
2023-09-23 21:32:03 +0200

    feat(Link.tsx): create Link component

---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,30 @@
+import { ReactElement } from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+
+import { SelectedPage } from '@/shared/types';
+
+type Props = {
+  page: string;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+export default function Link({
+  page,
+  selectedPage,
+  setSelectedPage,
+}: Props): ReactElement {
+  const lowerCasePage = page.toLowerCase().replace(/ /g, '') as SelectedPage;
+
+  return (
+    <AnchorLink
+      className={`${
+        selectedPage === lowerCasePage ? 'text-primary-500' : ''
+      } transition duration-500 hover:text-primary-300`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+}
