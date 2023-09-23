31b46e1022339c9af7c93a21224f5c9a05f2a2ed
Gildas Montcho <gmontcho@gmail.com>
2023-09-23 21:33:37 +0200

    feat(App.tsx): use Navbar in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { useState } from 'react';
+
+import Navbar from '@/scenes/navbar';
+
+import { SelectedPage } from './shared/types';
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+
   return (
     <div className="app bg-gray-20">
-      <h1>App</h1>
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
   );
 }
