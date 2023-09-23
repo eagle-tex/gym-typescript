dbc5116d86061b17905d83397a6b87037ace1aea
Gildas Montcho <gmontcho@gmail.com>
2023-09-23 11:14:29 +0200

    feat(src/scenes/navbar/index.tsx): start creating Navbar component
    at this stage, I created dummy components (Asset, Profile) and functions
    just to test the linting (eslint and typescript) setup

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -2,10 +2,15 @@
 import { useState } from 'react';
 
 // import { Profile } from '../../components/profile/Profile';
-import { Asset } from '@assets/Asset';
-import Logo from '@assets/Logo.png';
-import { Profile } from '@components/profile/Profile';
-import { sum } from '@helpers/functions';
+import { Asset } from '@/assets/Asset';
+import Logo from '@/assets/Logo.png';
+import { Profile } from '@/components/profile/Profile';
+import { sum } from '@/helpers/functions';
+
+// import { Asset } from '@assets/Asset';
+// import { Profile } from '@components/profile/Profile';
+// import Logo from '@assets/Logo.png';
+// import { sum } from '@helpers/functions';
 
 // import Logo from '../../assets/Logo.png';
 
