c79797e007c70f47ba90d42baecf1e5f1db5769b
Gildas Montcho <gmontcho@gmail.com>
2023-09-23 21:31:40 +0200

    feat(src/scenes/navbar/index.tsx): create Navbar component

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,30 +1,73 @@
-// import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 import { useState } from 'react';
 
-// import { Profile } from '../../components/profile/Profile';
-import { Asset } from '@/assets/Asset';
 import Logo from '@/assets/Logo.png';
-import { Profile } from '@/components/profile/Profile';
-import { sum } from '@/helpers/functions';
+import useMediaQuery from '@/hooks/useMediaQuery';
+import { SelectedPage } from '@/shared/types';
 
-// import { Asset } from '@assets/Asset';
-// import { Profile } from '@components/profile/Profile';
-// import Logo from '@assets/Logo.png';
-// import { sum } from '@helpers/functions';
+import Link from './Link';
 
-// import Logo from '../../assets/Logo.png';
+type Props = {
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-type Props = { dummy: string };
-
-function Navbar(props: Props) {
-  const { dummy } = props;
-
+function Navbar({ selectedPage, setSelectedPage }: Props) {
   const flexBetween = 'flex items-center justify-between';
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const isAboveMediumScreens = useMediaQuery('(min-width:1060px)');
 
   return (
     <nav>
       <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
-        Something {dummy}
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            {/* Left Side */}
+            <img src={Logo} alt="logo" />
+
+            {/* Right Side */}
+            {isAboveMediumScreens ? (
+              <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                  <p>Paragraph</p>
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Our Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Contact Us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+                <div className={`${flexBetween} gap-8`}>
+                  <p>Sign In</p>
+                  <p>Become a Member</p>
+                </div>
+              </div>
+            ) : (
+              <button
+                type="button"
+                className="rounded-full bg-secondary-500 p-2"
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
+              >
+                <Bars3Icon className="h-6 w-6 text-white" />
+              </button>
+            )}
+          </div>
+        </div>
       </div>
     </nav>
   );
