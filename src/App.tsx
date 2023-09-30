8f7d278692e80768eb72b7a10619a4c54a875f98
Gildas Montcho <gmontcho@gmail.com>
2023-09-30 07:42:27 +0200

    feat(App.tsx): use OurClasses component in App

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from '@/scenes/navbar';
 
 import Benefits from './scenes/benefits';
 import Home from './scenes/home';
+import OurClasses from './scenes/ourClasses';
 import { SelectedPage } from './shared/types';
 
 function App() {
@@ -36,6 +37,8 @@ function App() {
       <Home setSelectedPage={setSelectedPage} />
 
       <Benefits setSelectedPage={setSelectedPage} />
+
+      <OurClasses setSelectedPage={setSelectedPage} />
     </div>
   );
 }
